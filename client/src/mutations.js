aecbe6bff75900ab10366f8bc0217c8522f66f26
Liz <elizabeth.nino8813@gmail.com>
2023-06-27 01:31:28 -0500

    Fixing Mutations.js again

--- client/src/mutations.js
+++ client/src/mutations.js
@@ -26,7 +26,7 @@ export const ADD_USER = gql`
 
 export const SAVE_BOOK = gql`
   mutation saveBook($bookData: BookInput!) {
-    saveBook(bookInput: $BookData) {
+    saveBook(bookData: $bookData) {
       username
       bookData
     }
